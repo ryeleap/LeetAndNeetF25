92e439bf8a529d5bf501a99af90b1387e5edc328
Riley <pruittra@vcu.edu>
2025-08-11 18:26:55 -0400

    Added Reverse String

---
+++ reversestring.cs
@@ -0,0 +1,21 @@
+//7/31/25
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+        char temp;
+
+        while (left < right)
+        {
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
